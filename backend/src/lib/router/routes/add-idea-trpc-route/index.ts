eff81364126a0ce570c912649027e9bb8b9c472b
Anton Knysh <anton.knysh@andevart.com>
2025-05-12 18:39:46 +0300

    Restricted import code from BE to FE

--- backend/src/lib/router/routes/add-idea-trpc-route/index.ts
+++ backend/src/lib/router/routes/add-idea-trpc-route/index.ts
@@ -2,6 +2,8 @@ import { zValidationScheme } from '../../../../schemas/z-validation-schema';
 import { ideas } from '../../../mock-data';
 import { trpc } from '../../../trpc';
 
+export const xxx = 123;
+
 export const addIdeaTrpcRoute = trpc.procedure.input(zValidationScheme).mutation(({ input }) => {
   ideas.unshift(input);
 
