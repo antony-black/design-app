0b8eab5896aaa7e40eb15583ddc08163fe21e94c
Anton Knysh <anton.knysh@andevart.com>
2025-05-13 17:29:35 +0300

    Formated date-string to Date format

--- client/src/pages/idea-page/index.tsx
+++ client/src/pages/idea-page/index.tsx
@@ -1,8 +1,9 @@
-import { useParams } from 'react-router-dom';
-import styles from './index.module.scss';
 import { Segment } from '@/components';
 import type { TideaRouteParams } from '@/lib/routes';
 import { trpc } from '@/lib/trpc';
+import { format } from 'date-fns';
+import { useParams } from 'react-router-dom';
+import styles from './index.module.scss';
 
 const IdeaPage: React.FC = () => {
   const { nick } = useParams<TideaRouteParams>();
@@ -22,8 +23,11 @@ const IdeaPage: React.FC = () => {
     return <span>{error.message}</span>;
   }
 
+  const createdAt = format(data.idea.createdAt, 'yyyy-MM--dd');
+
   return (
     <Segment title={data.idea.name} description={data.idea.description}>
+      <div className={styles.createdAt}>{createdAt}</div>
       <div className={styles.text} dangerouslySetInnerHTML={{ __html: data.idea.text }} />
     </Segment>
   );
