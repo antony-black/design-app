148abbb221445a4619b3923380ce24f5e7dca4a1
Anton Knysh <anton.knysh@andevart.com>
2025-05-14 16:23:13 +0300

    Added "env.ts" for FE/rewrote "vite.config.ts"

--- client/src/lib/trpc.tsx
+++ client/src/lib/trpc.tsx
@@ -3,6 +3,7 @@ import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { httpBatchLink } from '@trpc/client';
 import { createTRPCReact } from '@trpc/react-query';
 import Cookies from 'js-cookie';
+import { env } from './env';
 
 export const trpc = createTRPCReact<TtrpcRouter>();
 
@@ -18,7 +19,7 @@ const queryClient = new QueryClient({
 const trpcClient = trpc.createClient({
   links: [
     httpBatchLink({
-      url: 'http://localhost:3000/trpc',
+      url: env.VITE_SERVER_TRPC_URL,
       headers: () => {
         const token = Cookies.get('token');
         return {
--- client/vite.config.ts
+++ client/vite.config.ts
@@ -1,39 +1,41 @@
-import * as path from 'node:path';
 import react from '@vitejs/plugin-react';
-import { defineConfig } from 'vitest/config';
+import * as path from 'node:path';
+import { defineConfig, loadEnv } from 'vite';
 import packageJson from './package.json' with { type: 'json' };
 
-// https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()],
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, process.cwd(), '');
 
-  resolve: {
-    alias: {
-      '@': path.resolve(__dirname, 'src'),
+  return {
+    plugins: [react()],
+    resolve: {
+      alias: {
+        '@': path.resolve(__dirname, 'src'),
+      },
     },
-  },
-
-  server: {
-    open: true,
-  },
-
-  build: {
-    outDir: 'build',
-    sourcemap: true,
-  },
-
-  test: {
-    root: import.meta.dirname,
-    name: packageJson.name,
-    environment: 'jsdom',
-
-    typecheck: {
-      enabled: true,
-      tsconfig: path.join(import.meta.dirname, 'tsconfig.json'),
+    server: {
+      port: +env.PORT,
     },
+    preview: {
+      port: +env.PORT,
+    },
+    build: {
+      outDir: 'build',
+      sourcemap: true,
+    },
+    test: {
+      root: import.meta.dirname,
+      name: packageJson.name,
+      environment: 'jsdom',
 
-    globals: true,
-    watch: false,
-    setupFiles: ['./src/setupTests.ts'],
-  },
+      typecheck: {
+        enabled: true,
+        tsconfig: path.join(import.meta.dirname, 'tsconfig.json'),
+      },
+
+      globals: true,
+      watch: false,
+      setupFiles: ['./src/setupTests.ts'],
+    },
+  };
 });
