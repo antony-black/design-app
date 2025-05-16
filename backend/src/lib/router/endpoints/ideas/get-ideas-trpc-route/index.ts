591e2421e92738dcbe5530f2db51496ec142e869
Anton Knysh <anton.knysh@andevart.com>
2025-05-16 19:17:11 +0300

    Added infinity ideas loading by click

--- backend/src/lib/router/endpoints/ideas/get-ideas-trpc-route/index.ts
+++ backend/src/lib/router/endpoints/ideas/get-ideas-trpc-route/index.ts
@@ -1,17 +1,23 @@
+import { zGetIdeasTrpcSchema } from '../../../../../schemas/z-get-ideas-schema';
 import { trpc } from '../../../../trpc';
 
-export const getIdeasTrpcRoute = trpc.procedure.query(async ({ ctx: appContext }) => {
+export const getIdeasTrpcRoute = trpc.procedure.input(zGetIdeasTrpcSchema).query(async ({ ctx: appContext, input }) => {
   const ideas = await appContext.prisma.idea.findMany({
     select: {
       id: true,
       nick: true,
       name: true,
       description: true,
+      serialNumber: true,
     },
-    orderBy: {
-      createdAt: 'desc',
-    },
+    orderBy: [{ createdAt: 'desc' }, { serialNumber: 'desc' }],
+    cursor: input.cursor ? { serialNumber: input.cursor } : undefined,
+    take: input.limit + 1,
   });
 
-  return { ideas };
+  const nextIdea = ideas.at(input.limit);
+  const nextCursor = nextIdea?.serialNumber;
+  const ideasExceptNext = ideas.slice(0, input.limit);
+
+  return { ideas: ideasExceptNext, nextCursor };
 });
--- client/src/pages/ideas/all-ideas-page/index.tsx
+++ client/src/pages/ideas/all-ideas-page/index.tsx
@@ -1,38 +1,59 @@
-import { Link } from 'react-router-dom';
-import styles from './index.module.scss';
-import { Segment } from '@/components';
+import { Notification, Segment } from '@/components';
 import * as routes from '@/lib/routes';
 import { trpc } from '@/lib/trpc';
+import { Link } from 'react-router-dom';
+import styles from './index.module.scss';
 
 export const AllIdeasPage = () => {
-  const { data, isLoading, isFetching, error } = trpc.getAllIdeas.useQuery();
-  if (isLoading || isFetching) {
-    return <span>Loading...</span>;
-  }
-  if (!data?.ideas) {
-    return <span>There are no ideas.</span>;
-  }
-  if (error) {
-    return <span>{error.message}</span>;
-  }
+  const { data, error, isLoading, isError, hasNextPage, fetchNextPage, isFetchingNextPage, isRefetching } =
+    trpc.getAllIdeas.useInfiniteQuery(
+      {
+        limit: 2,
+      },
+      {
+        getNextPageParam: (lastPage) => {
+          return lastPage.nextCursor;
+        },
+      },
+    );
 
   return (
     <Segment title="All Ideas">
-      <div className={styles.ideas}>
-        {data.ideas.map((idea) => (
-          <div className={styles.idea} key={idea.nick}>
-            <Segment
-              size={2}
-              title={
-                <Link className={styles.ideaLink} to={routes.getSingleIdeaRoute({ nick: idea.nick })}>
-                  {idea.name}
-                </Link>
-              }
-              description={idea.description}
-            />
+      {isLoading || isRefetching ? (
+        <div>Loading...</div>
+      ) : isError ? (
+        <Notification color="red">{error.message}</Notification>
+      ) : (
+        <div className={styles.ideas}>
+          {data.pages
+            .flatMap((page) => page.ideas)
+            .map((idea) => (
+              <div className={styles.idea} key={idea.nick}>
+                <Segment
+                  size={2}
+                  title={
+                    <Link className={styles.ideaLink} to={routes.getSingleIdeaRoute({ nick: idea.nick })}>
+                      {idea.name}
+                    </Link>
+                  }
+                  description={idea.description}
+                />
+              </div>
+            ))}
+          <div className={styles.more}>
+            {hasNextPage && !isFetchingNextPage && (
+              <button
+                onClick={() => {
+                  void fetchNextPage();
+                }}
+              >
+                Load more
+              </button>
+            )}
+            {isFetchingNextPage && <span>Loading...</span>}
           </div>
-        ))}
-      </div>
+        </div>
+      )}
     </Segment>
   );
 };
