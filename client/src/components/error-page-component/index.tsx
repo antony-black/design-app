eb45906ba4e7e23daaeee85eacedf0098d34ac13
Anton Knysh <anton.knysh@andevart.com>
2025-05-18 08:44:14 +0300

    Added image for the "NotFoundPage"

--- client/src/components/error-page-component/index.tsx
+++ client/src/components/error-page-component/index.tsx
@@ -3,15 +3,18 @@ import { Notification, Segment } from '@/components';
 type TErrorPageComponent = {
   title?: string;
   message?: string;
+  children?: React.ReactNode;
 };
 
 export const ErrorPageComponent: React.FC<TErrorPageComponent> = ({
   title = 'Oops, error',
   message = 'Something went wrong',
+  children,
 }) => {
   return (
     <Segment title={title}>
       <Notification color="red">{message}</Notification>
+      {children}
     </Segment>
   );
 };
--- client/src/pages/others/not-found-page/index.tsx
+++ client/src/pages/others/not-found-page/index.tsx
@@ -1,4 +1,6 @@
 import { ErrorPageComponent } from '@/components';
+import image404 from '../../../assets/images/404.png';
+import styles from './index.module.scss';
 
 type TNotFoundPage = {
   title?: string;
@@ -8,4 +10,8 @@ type TNotFoundPage = {
 export const NotFoundPage: React.FC<TNotFoundPage> = ({
   title = 'Not Found',
   message = 'This page does not exist',
-}) => <ErrorPageComponent title={title} message={message} />;
+}) => (
+  <ErrorPageComponent title={title} message={message}>
+    <img src={image404} alt="NOT_FOUND" className={styles.notFoundImage} width="800" height="600" />
+  </ErrorPageComponent>
+);
